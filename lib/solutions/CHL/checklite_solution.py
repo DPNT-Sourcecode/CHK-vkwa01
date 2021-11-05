e5c2c57942f3cb11d982992572e41155b9cb30fb
sbx_user1051 <sbx_user1051@169.254.10.57>
2021-11-05 16:14:26 +0000

    Fri Nov 05 16:14:26 UTC 2021

--- lib/solutions/CHL/checklite_solution.py
+++ lib/solutions/CHL/checklite_solution.py
@@ -1,15 +1,24 @@
 class Basket:
+    """
+    Helper class to calculate checkout
+    """
     prices = {
         "A": 50,
         "B": 30,
         "C": 20,
         "D": 15,
     }
+    offers = [
+        {"quantity": 3, "sku": "A", "price": 130},
+        {"quantity": 2, "sku": "B", "price": 45},
+    ]
 
     def __init__(self, skus: str):
         if any(sku not in prices for sku in skus):
-            raise ValueError()
+            raise ValueError(f"Invalid basket, all skus must be one of {', '.join(sku for sku in prices)}")
         self.skus = list(skus)
+
+    def calculate_checkout(self):
 
 
 # noinspection PyUnusedLocal
@@ -20,6 +29,8 @@ def checklite(skus: str) -> int:
     Args:
         skus: String where each letter represents an item in the basket.
     """
+    ...
+
 
 
 
