c990cb4bd7b8d52d63e21a22d428ea79f07e01e7
sbx_user1051 <sbx_user1051@169.254.10.57>
2021-11-05 16:08:26 +0000

    Fri Nov 05 16:08:26 UTC 2021

--- lib/solutions/CHL/checklite_solution.py
+++ lib/solutions/CHL/checklite_solution.py
@@ -2,5 +2,11 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checklite(skus):
+def checklite(skus: str) -> int:
+    """
+    Returns the value of the basket
+    Args:
+        skus: String where each letter represents an item in the basket.
+    """
     raise NotImplementedError()
+
