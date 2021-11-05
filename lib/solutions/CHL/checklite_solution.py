5f1c990147f83a1b8a638146b7b33ef8360821a8
sbx_user1051 <sbx_user1051@169.254.10.57>
2021-11-05 16:11:26 +0000

    Fri Nov 05 16:11:26 UTC 2021

--- lib/solutions/CHL/checklite_solution.py
+++ lib/solutions/CHL/checklite_solution.py
@@ -1,3 +1,15 @@
+class Basket:
+    prices = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15,
+    }
+
+    def __init__(self, skus: str):
+        if any(sku not in prices for sku in skus):
+            raise ValueError()
+        self.skus = list(skus)
 
 
 # noinspection PyUnusedLocal
@@ -8,5 +20,6 @@ def checklite(skus: str) -> int:
     Args:
         skus: String where each letter represents an item in the basket.
     """
-    raise NotImplementedError()
+
+
 
