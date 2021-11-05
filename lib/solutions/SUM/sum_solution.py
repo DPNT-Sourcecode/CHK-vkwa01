fb7e8189af9f3a18d7b07d1df4e32a6b2562d2e0
sbx_user1051 <sbx_user1051@169.254.106.213>
2021-11-05 15:49:45 +0000

    Fri Nov 05 15:49:45 UTC 2021

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,12 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(x: int, y: int) -> int:
+    """
+    Returns the sum of two integers
+    Args:
+        x: The first integer
+        y: The second integer
+    Returns:
+        The sum of the two integers passed.
+    """
+    return x + y
+
