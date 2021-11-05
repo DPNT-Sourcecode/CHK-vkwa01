763b80f5f3ba54d0f9c982fcebcba71fbf118211
sbx_user1051 <sbx_user1051@169.254.10.57>
2021-11-05 16:29:26 +0000

    Fri Nov 05 16:29:26 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,61 @@
+class Basket:
+    """
+    Helper class to calculate checkout
+    """
+    prices = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15,
+    }
+    offers = [
+        {"quantity": 3, "sku": "A", "price": 130},
+        {"quantity": 2, "sku": "B", "price": 45},
+    ]
+
+    def __init__(self, skus: str):
+        if any(sku not in prices for sku in skus):
+            raise ValueError(f"Invalid basket, all skus must be one of {', '.join(sku for sku in prices)}")
+        self.skus = sorted(list(skus))
+
+    def _calculate_offers_and_remove_skus(self) -> int:
+        """
+        Find offers in the basket and remove them from the list of skus.
+        Returns:
+            The total value of offers found.
+        """
+        offer_value = 0
+        for offer in self.offers:
+            quantity = offer["quantity"]
+            price = offer["price"]
+            sku = offer["sku"]
+            matches = self.skus.count(sku)
+            offers_found = matches // quantity
+            offer_value += offers_found * price
+            for _ in range(matches * quantity):
+                self.skus.remove(sku)
+        return offer_value
+
+    def calculate_checkout(self) -> int:
+        """
+        Returns
+        """
+        checkout_value = self._calculate_offers_and_remove_skus()
+        checkout_value += sum(self.prices[sku] for sku in self.skus)
+        return checkout_value
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    """
+    Returns the value of the basket
+    Args:
+        skus: String where each letter represents an item in the basket.
+    """
+    try:
+        basket = Basket(skus)
+        return basket.calculate_checkout()
+    except ValueError:
+        return -1
+
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 
+from lib.solutions.CHK.checkout_solution import ch
+
 
 class TestChk(TestCase):
-    def test_checklite__invalid_input(self):
+    def test_checklite__invalid_sku(self):
+        self.assertEqual()
+
 
